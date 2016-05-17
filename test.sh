4ab71b289ff63fae607aa32dd6ad8df49bf80ddc
Nícolas Peixoto <nicolaspeixoto93@gmail.com>
2016-05-17 15:37:42 -0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+curl -X POST -H "content-type: application/json" --data '{"name": "studygroup", "company": "ADP"}' localhost:3000/users/
+curl -X GET localhost:3000/users/
+
+#next steps
+curl -X GET localhost:3000/users/{id}
+curl -X PUT -H "content-type: application/json" --data '{"name": "studygroup", "company": "ADP"}' localhost:3000/users/1
+curl -X DELETE -H localhost:3000/users/1
